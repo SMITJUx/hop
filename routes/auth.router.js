6448cb949b0715a493d05170a187ec46a232212f
Samir J <smitju@proton.me>
2023-04-26 22:53:58 +0200

    refactor: Improve authentication routes.

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const authenticate = require('../middleware/auth.middleware')
 const controller = require('../controllers/auth.controller')
 
-router.get('/', authenticate.verifyUserLocal, controller.login)
-router.post('/', controller.register)
+router.post('/login', authenticate.verifyUserLocal, controller.login)
+router.post('/register', controller.register)
 router.get('/refresh', authenticate.verifyUserJwtRefresh, controller.refresh)
 
 module.exports = router
