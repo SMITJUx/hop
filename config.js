80ea0c506324b06cbeb544407108e821d692077e
Samir J <smitju@proton.me>
2023-04-26 22:53:58 +0200

    refactor: API throttling configuration.

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ config.api = {
     key: process.env.API_KEY || '',
 }
 
-config.limit = {
+config.rateLimit = {
     windowMs: 15 * 60 * 1000, // 15 minutes,
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers,
