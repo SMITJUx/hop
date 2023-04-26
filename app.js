82456eb0da2540d92885ef258babdbeb597df891
Samir J <smitju@proton.me>
2023-04-26 22:53:57 +0200

    refactor: Move throttling middleware config.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const logger = require('morgan')
 const passport = require('passport')
 const helmet = require('helmet')
 const cors = require('cors')
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('./middleware/limit.middleware').rateLimit
 const authRouter = require('./routes/auth.router')
 const authenticate = require('./middleware/auth.middleware')
 const LocalStrategy = require('passport-local').Strategy
@@ -22,14 +22,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(cors())
 app.use(helmet())
-app.use(
-    rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-    }),
-)
+app.use(rateLimit)
 app.use(passport.initialize())
 
 passport.use(new LocalStrategy(User.authenticate()))
