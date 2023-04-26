9960c5ffd7ad82974ff11a2d14b5ad52e520605d
Samir J <smitju@proton.me>
2023-04-26 22:53:58 +0200

    feature: Define swagger schemas.

--- models/refreshToken.model.js
+++ models/refreshToken.model.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose')
 const expireIn = require('../config').auth.expireIn
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     RefreshToken:
+ *       type: object
+ *       required:
+ *         - userId
+ *         - value
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The auto-generated id of the travel
+ *         userId:
+ *           type: string
+ *           description: User id related to this refresh token
+ *         value:
+ *           type: string
+ *           description: Refresh token value
+ *         revoked:
+ *           type: string
+ *           description: Date when the refresh token was invalidated
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the refresh token was added
+ *         updatedAt:
+ *           type: string
+ *           format: date
+ *           description: The last date when the refresh token was updated
+ */
 const RefreshTokenSchema = new mongoose.Schema(
     {
         userId: {
--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,45 @@ const mongoose = require('mongoose')
 const passportLocalMongoose = require('passport-local-mongoose')
 const roles = require('../config').roles
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *         - email
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         username:
+ *           type: string
+ *           description: User username
+ *         password:
+ *           type: string
+ *           description: User password
+ *         email:
+ *           type: string
+ *           description: User email
+ *         roles:
+ *           type: array
+ *           description: Roles of the user
+ *         revoked:
+ *            type: string
+ *            format: date
+ *            description: Date when this user got banned
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the user was added
+ *         updatedAt:
+ *           type: string
+ *           format: date
+ *           description: The last date when the user was updated
+ */
 const UserSchema = new mongoose.Schema(
     {
         username: {
@@ -11,6 +50,7 @@ const UserSchema = new mongoose.Schema(
         },
         password: {
             type: String,
+            required: true,
         },
         email: {
             type: String,
